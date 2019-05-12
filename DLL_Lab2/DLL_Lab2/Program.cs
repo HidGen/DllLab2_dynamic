02c76526e10b997394d17a8dace0025b9fad7d01
Spikehero <44679068+Spikehero@users.noreply.github.com>
2019-05-12 16:31:32 +0300

    Лаба с длл с динамическим подключением

---
+++ DLL_Lab2/DLL_Lab2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DLL_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.Load("Lab2_Library");
+            Object instance = assembly.CreateInstance("Class1");
+            var type = assembly.GetType("Class1");
+            MethodInfo method = type.GetMethod("Add");
+
+            Console.Write("Введите x: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите y: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            var numbers = new object[2];
+            numbers[0] = b;
+            numbers[1] = c;
+
+          //  var result = method.Invoke(instance, numbers);
+
+            Console.WriteLine(method.Invoke(instance, numbers));
+
+            MethodInfo meth = type.GetMethod("Sub");
+         //   var result1 = meth.Invoke(instance, numbers);
+            Console.WriteLine(meth.Invoke(instance, numbers));
+
+            Console.ReadLine();
+        }
+    }
+}
+/*add и sub*/
---
+++ Lab2_Library/Lab2_Library/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Class1
+    {
+      public double Add(double x, double y)
+      {
+     //   Console.WriteLine( Convert.ToString(x) + Convert.ToString(y));
+            return Math.Round(0.05 *( Math.Sin(x*y)), 2);
+      }
+
+      public double Sub(double x, double y)
+      {
+   //     Console.WriteLine(Convert.ToString(x) + Convert.ToString(y));
+        return Math.Round(((x-y) / (x*y)), 2);
+      }
+    }
+
